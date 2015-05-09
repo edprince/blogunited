fbc7188be0170c7cc66fbb538b9dd6f5c9dd64a6
ed <edward_prince@hotmail.com>
2015-05-09 13:01:24 +0100

    starting up

---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<head>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.2/css/foundation.css" />
+  <link href="assets/styles/style.css" />
+</head>
---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+  <?php require("includes/header.php"); ?>
+<body>
+  <nav>
+</body>
+</html>
